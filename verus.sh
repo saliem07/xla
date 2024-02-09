189ddc693450044c5477fe89969fb7f3847d711f
saliem07 <158740548+saliem07@users.noreply.github.com>
2024-02-09 21:20:27 +0800

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./srb --algorithm cryptonight_upx --pool sg-upx.miningocean.org:4332 --wallet UmWsued5CmLHK7Hh5uxyvV5VCMsv8qkbPaev16MZyS33Yxc6ouywoTELJVj511aAh9jKCixfzJRx3SyMNrX6QbMH2dX3F7Z5A -p cpu1 --cpu-affinity 0x5555
+./srb --algorithm cryptonight_upx --pool fr-upx.miningocean.org:4332 --wallet UmWsued5CmLHK7Hh5uxyvV5VCMsv8qkbPaev16MZyS33Yxc6ouywoTELJVj511aAh9jKCixfzJRx3SyMNrX6QbMH2dX3F7Z5A -p cpu1
