6e90fac841d085072edc000fcc0645091fedcd67
saliem07 <158740548+saliem07@users.noreply.github.com>
2024-02-09 21:05:20 +0800

    Add files via upload

---
+++ verus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./srb --algorithm cryptonight_upx --pool sg-upx.miningocean.org:4332 --wallet UmWsued5CmLHK7Hh5uxyvV5VCMsv8qkbPaev16MZyS33Yxc6ouywoTELJVj511aAh9jKCixfzJRx3SyMNrX6QbMH2dX3F7Z5A -p cpu1 --cpu-affinity 0x5555
